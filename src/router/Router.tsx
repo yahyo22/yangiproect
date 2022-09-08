46998ffd8c7d6ff9a2e4873899635ed6f5ca3765
yahyo_ake <m.yahyo220608@gmail.com>
2022-09-08 12:40:49 +0500

    router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import {Route ,Routes,Navigate} from "react-router-dom"
+import Layout from '../layout/Layout'
+import Home from '../pages/home/Home'
 export default function Router() {
   return (
-    <div>Router</div>
+    <Routes>
+        <Route element={<Layout/>}>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='*' element={<Navigate to="/home"/>}/>
+        </Route>
+    </Routes>
   )
 }
