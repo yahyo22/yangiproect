62eeee9ab30b1d1e0ee4fead7c3f4cbca6391017
Jasur <jasurbekshomaqsudov2004@gmail.com>
2022-09-08 12:41:35 +0300

    navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// Images
+import logo from "../../assets/img/logo.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+        <div className="container">
+            <ul>
+                <li>
+                    <Link to="#">
+                        <img src={logo} alt="logo" />
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
