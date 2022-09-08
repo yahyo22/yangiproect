555f31a24770bd93164cbc12fc560f938853f1ec
yahyo_ake <m.yahyo220608@gmail.com>
2022-09-08 15:22:30 +0500

    uzbek tayor

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import Cardd from "../../components/card/Cardd";
-
+import maktab from "../../assets/img/maktab.png";
+import maktab2 from "../../assets/img/maktab2.png";
+import maktab3 from "../../assets/img/maktab3.png";
+import uzbekistan from "../../assets/img/uzbekistan.png";
 export default function Home() {
   var Carousel = require("3d-react-carousal").Carousel;
   let slides = [
-    <img src="https://picsum.photos/800/300/?random" alt="1" />,
-    <img src="https://picsum.photos/800/301/?random" alt="2" />,
-    <img src="https://picsum.photos/800/302/?random" alt="3" />,
-    <img src="https://picsum.photos/800/303/?random" alt="4" />,
-    <img src="https://picsum.photos/800/304/?random" alt="5" />,
+    <img src={maktab} alt="1" />,
+    <img src={maktab2} alt="2" />,
+    <img src={maktab3} alt="3" />,
   ];
   return (
     <StyledHome>
@@ -18,16 +19,39 @@ export default function Home() {
           <div className="cards">
             <Cardd />
           </div>
-
         </div>
-
-        {/* CARRUSEL SECTION */}
-        <section className="carrusel__wrapper">
+        <div className="main-2">
+          {/* <section className="carrusel__wrapper">
           <h1>Maktablar</h1>
           <main className="carrusel-main">
-            <Carousel slides={slides} autoplay={true} interval={5000} />
+            <Carousel slides={slides} autoplay={true} interval={100000} />
           </main>
+        </section> */}
+        <section className="uzbekistan">
+          <section>
+            <img src={uzbekistan} alt="uzbek" />
+          </section>
+          <section className="uzbek-2">
+            <div>
+              <h2>Davlat maktablari</h2>
+              <p>12 000+</p>
+            </div>
+            <div>
+              <h2>Prezident maktablari</h2>
+              <p>23+</p>
+            </div>
+            <div>
+              <h2>Ijod maktablari</h2>
+              <p>345+</p>
+            </div>
+            <div>
+              <h2>Xususiy maktablar </h2>
+              <p>56+</p>
+            </div>
+          </section>
         </section>
+        </div>
+        
       </div>
     </StyledHome>
   );
@@ -39,14 +63,53 @@ const StyledHome = styled.div`
 
     .wrapper-home {
       .cards {
-      margin: 40px 0px;
+        margin: 40px 0px;
         display: grid;
         grid-template-columns: repeat(3, 1fr);
         gap: 24px;
       }
-      .carusel{
-        background: #FFFFFF;
+    }
+    .main-2{
+      background: #FFFFFF;
+    .uzbekistan {
+      margin-top: 115px;
+      align-items: center;
+      display: flex;
+      justify-content: space-between;
+      flex-direction: column;
+      gap: 23px;
+
+      .uzbek-2 {
+        
+      background: #FFFFFF;
+border: 1px solid rgba(49, 57, 73, 0.1);
+border-radius: 24px;
+        gap: 46px;
+        display: flex;
+        padding: 40px 32px;
+        div {
+          display: flex;
+          flex-direction: column;
+          gap: 48px;
+          text-align: center;
+          h2 {
+            font-style: normal;
+            font-weight: 500;
+            font-size: 24px;
+            line-height: 32px;
+            color: #12aae4;
+          }
+          p {
+            font-family: "Montserrat";
+            font-style: normal;
+            font-weight: 600;
+            font-size: 40px;
+            line-height: 48px;
+            color: #12aae4;
+          }
+        }
       }
+    }
     }
   }
 `;
