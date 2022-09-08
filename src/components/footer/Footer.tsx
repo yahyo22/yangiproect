6845249f9d1aa47a490c46457cb38e6d521a609d
yahyo_ake <m.yahyo220608@gmail.com>
2022-09-08 13:59:22 +0500

    footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+export default function Footer() {
+  return (
+    <StyledFooter>
+
+    </StyledFooter>
+  )
+}
+const StyledFooter = styled.div`
+        
+`
