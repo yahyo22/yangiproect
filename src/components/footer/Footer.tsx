f300ca0a8bf4b9b0fc17cc14a35b5432c3e57ade
yahyo_ake <m.yahyo220608@gmail.com>
2022-09-08 16:55:08 +0500

    kdmkcmdskvmkfv

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,56 +1,104 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 export default function Footer() {
   return (
     <StyledFooter>
-        <div className="container">
+      <div className="container">
         <section>
+          <div>
+            <h1>Yangiliklar</h1>
+            <ul>
+              <li>O‘qituvchilar</li>
+              <li>O‘quvchilar</li>
+              <li>Ota-onalar</li>
+            </ul>
+          </div>
+          <div>
+            <h1>Maktablar</h1>
+            <ul>
+              <li>Davlat maktablari</li>
+              <li>Prezident maktablari</li>
+              <li>Ijod maktablari</li>
+              <li>Xususiy maktablar</li>
+            </ul>
+          </div>
+          <div>
+            <h1>Olimpiyadalar</h1>
+            <ul>
+              <li>Davlat olimpiyadalari</li>
+              <li>Xalqaro olimpiyadalar</li>
+              <li>Xalqaro tanlovlar</li>
+              <li>Xususiy olimpiyadalar</li>
+            </ul>
+          </div>
+          <div>
+            <h1>Kutubxona</h1>
+            <ul>
+              <li>Testlar</li>
+              <li>Imtihon</li>
+              <li>Hujjatlar</li>
+            </ul>
+          </div>
+          <div>
             <div>
-                <h2>Yangiliklar</h2>
-                <ul>
-                    <li>O‘qituvchilar</li>
-                    <li>O‘quvchilar</li>
-                    <li>Ota-onalar</li>
-                </ul>
+              <i className="icon mail"></i>
+              <p>Registon.@gmail.uz</p>
             </div>
             <div>
-            <h2>Maktablar</h2>
-                <ul>
-                    <li>Davlat maktablari</li>
-                    <li>Prezident maktablari</li>
-                    <li>Ijod maktablari</li>
-                    <li>Xususiy maktablar</li>
-                </ul>
+              <i className="icon phone"></i>
+              <p>+998 90 010-85-10</p>
             </div>
             <div>
-            <h2>Olimpiyadalar</h2>
-                <ul>
-                    <li>Davlat olimpiyadalari</li>
-                    <li>Xalqaro olimpiyadalar</li>
-                    <li>Xalqaro tanlovlar</li>
-                    <li>Xususiy olimpiyadalar</li>
-                </ul>
+              <p>Ijtimoiy tarmoqlarimiz:</p>
+              <div>
+                <div className="facebook"></div>
+                <div className="icon telegram"></div>
+                <div className="icon insta"></div>
+                <div className="icon twiter"></div>
+              </div>
             </div>
-            <div>
-            <h2>Kutubxona</h2>
-                <ul>
-                    <li>Testlar</li>
-                    <li>Imtihon</li>
-                    <li>Hujjatlar</li>
-                </ul>
-            </div>
-            <div>
-                <div>
-                    <i className='icon insta'></i>
-                </div>
-            </div>
+          </div>
         </section>
         <section></section>
-        </div>
-        
+      </div>
     </StyledFooter>
-  )
+  );
 }
 const StyledFooter = styled.div`
-    background: #12AAE4;
-`
+  background: #12aae4;
+  section {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    div {
+      ul {
+        list-style: circle;
+        li {
+          color: #ffffff;
+          font-size: 16px;
+          font-weight: 400;
+          line-height: 24px;
+          letter-spacing: 0em;
+          text-align: left;
+        }
+      }
+      h1 {
+        font-size: 18px;
+        font-weight: 500;
+        line-height: 24px;
+        letter-spacing: 0em;
+        text-align: left;
+        color: #ffffff;
+      }
+      p {
+        font-family: Montserrat;
+        font-size: 18px;
+        font-weight: 500;
+        line-height: 24px;
+        letter-spacing: 0em;
+        text-align: left;
+        color: #ffffff;
+      }
+    }
+  }
+`;
